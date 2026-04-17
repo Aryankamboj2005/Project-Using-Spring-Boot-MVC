240c070f4ed352d50338fc67fd432ea930f2a397
Aryan <Aryankamboj2005@gmail.com>
2026-04-17 11:22:19 +0530

    Implement ResponseEntity in ProductController for improved HTTP status responses

--- src/main/java/com/Aryan/Ecom_Project/Controller/ProductController.java
+++ src/main/java/com/Aryan/Ecom_Project/Controller/ProductController.java
@@ -3,30 +3,38 @@ package com.Aryan.Ecom_Project.Controller;
 import com.Aryan.Ecom_Project.Model.Product;
 import com.Aryan.Ecom_Project.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin 
-@RestController 
-@RequestMapping("/api") 
+@CrossOrigin
+@RestController
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/") 
+    @RequestMapping("/")
     public String greet() {
         return "Hello World";
     }
 
     @GetMapping("/products") 
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/product/{id}")
-    public Product getProductById(@PathVariable int id) {
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
